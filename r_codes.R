8a14b17946954f2fdf42d1e641286be59b28d140
jovialman <jovialman87@gmail.com>
2014-08-18 18:02:44 +0530

    Initial set of codes

---
+++ r_codes.R
@@ -0,0 +1,12 @@
+getwd()
+data()
+
+data <- mtcars  # using the mtcars data available in the base package
+
+# exploring the data set
+names(data)  # names of the columns
+str(data)  # type of data
+class(data)  # structure of the data
+dim(data)  # number of rows and columns
+head(data)  # first few rows
+tail(data)  # last few rows
